1687628f77463eaa55b120a5bf55d189dd9a56e0
Arpit Saini <69818002+codeme13@users.noreply.github.com>
2021-08-02 12:44:29 +0530

    Add files via upload

---
+++ models/all.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+
+
+const databSchema = new mongoose.Schema({
+    Date: Date,
+    payment: String
+});
+
+const Datab = mongoose.model("Datab", databSchema);
+
+module.exports = Datab;
