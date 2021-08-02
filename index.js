9be1026ce7760fe075334bfc841dacc73cb8a71d
Arpit Saini <69818002+codeme13@users.noreply.github.com>
2021-08-02 12:42:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,158 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate")
+const Customer = require("./models/user");
+const Transaction = require("./models/transaction");
+const Datab = require("./models/all");
+const AppError = require("./views/AppError");
+require("./db");
+const request = require('request');
+
+
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set(path.join(__dirname, "/views"));
+
+app.engine("ejs", ejsMate);
+
+function wrapAsync(fn) {
+    return function(req, res, next) {
+        fn(req, res, next).catch(e => next(e));
+    }
+};
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/customer", wrapAsync(async(req, res, next) => {
+    const customer = await Customer.find({});
+    if (!customer) {
+        throw new AppError("No users exist", 401);
+    }
+    res.render("customers", { customer });
+}));
+
+app.get("/customer/:id/transfer", wrapAsync(async(req, res, next) => {
+    const { id } = req.params;
+    const cs = await Customer.findById(id);
+    const cus = await Customer.find({});
+    if (!cs || !cus) {
+        throw new AppError("User Not Found", 401);
+    }
+    res.render("transfer", { cus, cs });
+}));
+
+app.get("/customer/:id/history", wrapAsync(async(req, res, next) => {
+    const { id } = req.params;
+    const c = await Customer.findById(id);
+    const v = c.username;
+    if (!v) {
+        throw new AppError("User Not Found", 401);
+    }
+    await Customer.findOne({ username: v })
+        .populate("transactions")
+        .exec(function(err, t) {
+            t.toObject({ getters: true }); //to convert into the object or Json
+            res.render("history", { t });
+        });
+}));
+
+app.get("/customer/:id", wrapAsync(async(req, res, next) => {
+    const { id } = req.params;
+    const c = await Customer.findById(id);
+    const v = c.username;
+    if (!v) {
+        throw new AppError("User Not Found", 401);
+    }
+    await Customer.findOne({ username: v })
+        .populate("transactions")
+        .exec(function(err, t) {
+            t.toObject({ getters: true }); //to convert into the object or Json
+            res.render("show", { t });
+        });
+}));
+app.get("/add",async(req,res)=>{
+   
+  res.render('add'); 
+
+})
+app.post("/add",async(req,res)=>{
+    const{name,contact,amount,place} = req.body;
+    const email = "arpit@gmail.com";
+    const avatar ="https://joeschmoe.io/api/v1/{name}"
+    const user = {
+        username:name,
+        email:email,
+        Balance:amount, 
+        avatar:avatar
+    }; 
+    
+
+
+    try{
+        const data=new Customer(user);
+        console.log(data);
+        const dum= await data.save();
+        res.render("home");
+         }
+         catch(err)
+         {
+       res.status(400). send(err);
+         }
+
+})
+
+app.post("/customer", wrapAsync(async(req, res, next) => {
+    const { username, Amount, From, comment } = req.body;
+    const cus = await Customer.findOne({ username: username }); //using findOne Instead Of find so that it return an object not array
+    const b = await Customer.findOne({ username: From });
+    if (!cus || !b) {
+        throw new AppError("User Not Found", 401);
+    }
+    if (b.Balance > 0 && Amount < b.Balance && Amount > 0) {
+        const f = new Transaction({ Date: Date(), amount: Amount, Description: `Recieved $${Amount} from ${b.username}`, comment: `${comment}` });
+        await f.save();
+        const t = new Transaction({ Date: Date(), amount: Amount, Description: `Paid $${Amount} to ${cus.username}`, comment: `${comment}` });
+        await t.save() //If we want to use populate we need to use this
+        cus.transactions.push(f);
+        await cus.save();
+        const d = new Datab({ Date: Date(), payment: `${b.username} sends $${Amount} to ${cus.username}` });
+        await d.save();
+        b.transactions.push(t);
+        await b.save();
+        const am = parseInt(cus.Balance) + parseInt(Amount);
+        await Customer.findOneAndUpdate({ username: From }, { Balance: parseInt(b.Balance)+1 - parseInt(Amount) });
+        await Customer.findOneAndUpdate({ username: username }, { Balance: am });
+        res.redirect("/");
+    } else if (Amount > b.Balance) { 
+        throw new AppError("You Haven't enough Balance to make payment", 500);
+    } else {
+        throw new AppError("Amount should be positive", 500);
+    }
+}));
+
+app.get("/transactions", async(req, res) => {
+    const a = await Datab.find({});
+    res.render("transactions", { a });
+});
+
+app.all("*", (req, res, next) => {
+    next(new AppError("Page Not Found", 404));
+});
+
+app.use((err, req, res, next) => {
+    const { status = 401, message = "Something wrong Occured" } = err;
+    if (!err.message)
+        err.message = "Something Wrong";
+    res.status(status).render("error", { err });
+});
+
+app.listen(process.env.PORT, (req, res) => {
+    console.log("Welcome to the Server")
+});
